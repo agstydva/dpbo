1413253d7e732d177783a3914994600ec5f9b3e6
agstydva <agastyadava01@gmail.com>
2024-12-14 18:26:01 +0700

    Create home_screen Page Layout And Structure

--- my_app/lib/screens/home_screen.dart
+++ my_app/lib/screens/home_screen.dart
@@ -4,14 +4,71 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
-      ),
-      body: Center(
-        child: Text(
-          "Welcome to Home Page",
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 0, 164, 3),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.account_circle_rounded,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.menu,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.only(left: 3, bottom: 15),
+                  child: Text(
+                    "Hi, Agastya Dava",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, bottom: 20),
+                  width: MediaQuery.of(context).size.width,
+                  height: 35,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search here....",
+                      hintStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      prefixIcon: Icon(Icons.search, size: 25),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
