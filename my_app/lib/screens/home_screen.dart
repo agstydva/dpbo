65685195298ddd15e919e7304a728984efd87ab3
agstydva <agastyadava01@gmail.com>
2024-12-14 22:16:13 +0700

    Fixed Error in Category Feature

--- my_app/lib/screens/home_screen.dart
+++ my_app/lib/screens/home_screen.dart
@@ -235,7 +235,7 @@ class HomePage extends StatelessWidget {
                           ),
                         ],
                       ),
-                    ),
+                    );
                   },
                 ),
                 Row(
