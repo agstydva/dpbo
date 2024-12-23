746c98fea73fb0080c2363cd39ce535c2fd0ffd5
KidboyMC <57256311+KidboyMC@users.noreply.github.com>
2024-12-23 07:33:17 +0700

    Update location_page.dart

--- my_app/lib/screens/location_page.dart
+++ my_app/lib/screens/location_page.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class LocationPage extends StatelessWidget {
   const LocationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Location Page"),
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(-6.2088, 106.8456), // Center the map over London
+        initialZoom: 13.0,
       ),
-      body: const Center(
-        child: Text(
-          "Welcome to Location Page",
-          style: TextStyle(fontSize: 20),
+      children: [
+        TileLayer( // Display map tiles from any source
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+          userAgentPackageName: 'com.example.app',
+          // And many more recommended properties!
         ),
-      ),
+        RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
+            ),
+            // Also add images...
+          ],
+        ),
+      ],
     );
   }
 }
